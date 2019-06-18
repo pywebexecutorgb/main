310b04c35ff928973c491583319b5f3e6535142a
Andrey Kiselev <ezhichek@yandex-team.ru>
2019-06-18 12:29:07 +0300

    update mainapp models

--- executor/mainapp/models.py
+++ executor/mainapp/models.py
@@ -1,59 +1,83 @@
 '''
-> output, profile = [], []
-> command = """
-> import requests
-> try:
->     print(requests.get("http://www.yandex.ru/").status_code)
-> except Exception as err:
->     print(err)
-> """
+How to try execute code and store it:
 
-> from mainapp.utils import DockerExec
-> with DockerExec('python3', command, "requests") as exec:
->     profile_flag = False
->     for msg in exec:
->         if 'function calls' in msg:
->             profile_flag = True
->         if profile_flag:
->             profile.append(msg)
->             continue
->         output.append(msg)
->         print(msg)
-    200
+from mainapp.models import CodeBase, CodeExecution
+from mainapp.utils import DockerExec
+codebase = CodeBase(interpreter=CodeBase.INTERPRETER_PYTHON3, code_text="""
+import requests
+try:
+    print(requests.get("http://www.yandex.ru/").status_code)
+except Exception as err:
+    print(err)
+""", dependencies="requests")
+output, profile = [], []
+codebase.save()
 
-> from mainapp.models import CodeBase, CodeExecution
-> codebase = CodeBase(code_text=command)
-> codebase.save()
-> codebase
-    <CodeBase: 1 (
+code_exec = None
+with DockerExec(codebase.get_interpreter_display(), codebase.code_text, codebase.dependencies) as exec:
+    if exec.stderr:
+        code_exec = CodeExecution(code=codebase, has_errors=True, output=exec.stderr, profile='')
+    else:
+        profile_flag = False
+        for line in exec.stdout.split('\n'):
+            if 'function calls' in line:
+                profile_flag = True
+            if profile_flag:
+                profile.append(line)
+                continue
+            output.append(line)
+        code_exec = CodeExecution(code=codebase, has_errors=False, output='\n'.join(output), profile='\n'.join(profile))
+    code_exec.save()
+
+codebase
+    <CodeBase: 1
+                    interpreter='python3'
+                    code='
     import requests
     try:
-        print(, False)>
+        print(requests.get("http://www.yandex.'
+                    dependencies='requests'>
 
-> code_exec = CodeExecution(code=codebase, output='\n'.join(output), profile='\n'.join(profile))
-> code_exec.save()
-> code_exec
-    <CodeExecution: 1 (
+code_exec
+    <CodeExecution: 4:
+                    code='
     import requests
     try:
-        print(, 200,          190125 function calls ()>
+        print(',
+                    has_errors='False'
+                    output='200',
+                    profile='         190188 function calls ('>
 '''
 
 from django.db import models
 
 
 class CodeBase(models.Model):
+    INTERPRETER_PYTHON2 = 2
+    INTERPRETER_PYTHON3 = 3
+
+    INTERPRETER_CHOICES = (
+        (INTERPRETER_PYTHON2, 'python'),
+        (INTERPRETER_PYTHON3, 'python3'),
+    )
+
     class Meta:
         verbose_name = "User's code"
         verbose_name_plural = "User's code"
 
+    interpreter = models.PositiveSmallIntegerField(verbose_name='Python interpreter version',
+                                                   choices=INTERPRETER_CHOICES,
+                                                   default=INTERPRETER_PYTHON3)
     code_text = models.CharField(verbose_name='Code text', max_length=2048)
-    is_errors = models.BooleanField(verbose_name='Errors', default=False)
+    dependencies = models.CharField(verbose_name='requirements.txt', max_length=256,
+                                    null=True, blank=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
-    processed_at = models.DateTimeField(null=True, default=None)
 
     def __str__(self):
-        return f"{self.pk} ({self.code_text[:32]}, {self.is_errors})"
+        return f"""{self.pk}
+                interpreter='{self.get_interpreter_display()}'
+                code='{self.code_text[:64]}'
+                dependencies='{self.dependencies[:32]}'"""
 
 
 class CodeExecution(models.Model):
@@ -61,9 +85,17 @@ class CodeExecution(models.Model):
         verbose_name = "User's code result"
         verbose_name_plural = "User's code results"
 
-    code = models.ForeignKey(CodeBase, related_name='code', on_delete=models.CASCADE)
+    code = models.ForeignKey(CodeBase, related_name='Code', on_delete=models.CASCADE)
+    processed_at = models.DateTimeField(verbose_name='Execution timestamp', auto_now_add=True)
+    has_errors = models.BooleanField(verbose_name='Does code have errors?', default=False)
+
     output = models.CharField(verbose_name='Code execution result', max_length=2048)
-    profile = models.CharField(verbose_name='Code profile result', max_length=2048)
+    profile = models.CharField(verbose_name='Code profile result', max_length=2048,
+                               null=True, default=None)
 
     def __str__(self):
-        return f"{self.pk} ({self.code.code_text[:32]}, {self.output[:32]}, {self.profile[:32]})"
+        return f"""{self.pk}:
+                code='{self.code.code_text[:32]}',
+                has_errors='{self.has_errors}'
+                output='{self.output[:32]}',
+                profile='{self.profile[:32]}'"""
