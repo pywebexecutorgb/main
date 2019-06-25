4ae56d38382e08825a0423a612590733e1e9e4a3
v-v-d <v-v-d@bk.ru>
2019-06-25 15:24:37 +0300

    1 add create, read CBV to mainapp/views.py 2 add form for create to mainapp/forms.py 3 add urls to mainapp/urls.py for CBV 4 add code_read.html to mainapp/templates/mainapp for models data test display 5 add form.as_p to main_content.html

---
+++ executor/mainapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from .models import CodeBase
+
+
+class CodeBaseForm(forms.ModelForm):
+    class Meta:
+        model = CodeBase
+        exclude = ('created_at',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
