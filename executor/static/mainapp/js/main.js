b187a90abe78d60a13705d947db6428e052b0f3b
Andrey Kiselev <ezhichek@yandex-team.ru>
2019-06-25 19:31:07 +0300

    funny changes

--- executor/static/mainapp/js/main.js
+++ executor/static/mainapp/js/main.js
@@ -10,3 +10,18 @@ btn.addEventListener('click', (e) => {
     e.preventDefault();
     resetInputForm(e);
 });
+
+let outputResult = document.getElementById('output-result');
+setInterval(function () {
+    if (!outputResult.classList.contains('blinking')) {
+        return;
+    }
+
+    if (outputResult.classList.contains('on')) {
+        outputResult.textContent = null;
+        outputResult.classList.remove('on');
+    } else {
+        outputResult.textContent = '>';
+        outputResult.classList.add('on');
+    }
+}, 1000);
