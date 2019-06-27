48dc9cb0653a5ab5916b283f272854a4705b6c41
Andrey Kiselev <ezhichek@yandex-team.ru>
2019-06-27 16:45:48 +0300

    add spinner

--- executor/static/mainapp/js/main.js
+++ executor/static/mainapp/js/main.js
@@ -1,25 +1,25 @@
 "use strict";
 
 function resetInputForm(event) {
-    document.getElementById('id_code_text').textContent = '';
-    document.getElementById('id_dependencies').textContent = '';
+  document.getElementById('id_code_text').textContent = '';
+  document.getElementById('id_dependencies').textContent = '';
 }
 
 let btn = document.querySelector('.clear-button');
 btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    resetInputForm(e);
+  e.preventDefault();
+  resetInputForm(e);
 });
 
 let outputResult = document.getElementById('output-result');
 if (outputResult.classList.contains('blinking')) {
-    setInterval(function () {
-        if (outputResult.classList.contains('on')) {
-            outputResult.innerHTML = '&#9646;';
-            outputResult.classList.remove('on');
-        } else {
-            outputResult.innerHTML = '&#9647;';
-            outputResult.classList.add('on');
-        }
-    }, 700);
+  setInterval(function () {
+    if (outputResult.classList.contains('on')) {
+      outputResult.innerHTML = '&#9646;';
+      outputResult.classList.remove('on');
+    } else {
+      outputResult.innerHTML = '&#9647;';
+      outputResult.classList.add('on');
+    }
+  }, 700);
 }
