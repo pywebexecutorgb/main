fbbcd2fc4c75edd742a7f41a3d1608a75b2f5966
Andrey Kiselev <ezhichek@yandex-team.ru>
2019-07-02 12:51:53 +0300

    create copy button

--- executor/mainapp/urls.py
+++ executor/mainapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from mainapp.views import CodeCreate, CodeRead, code_read_ajax
+from mainapp.views import CodeCreate, CodeRead
 
 app_name = 'mainapp'
 urlpatterns = [
     path('create/', CodeCreate.as_view(), name='index'),
     path('read/<int:pk>', CodeRead.as_view(), name='read'),
-    path('ajax_read/<int:pk>', code_read_ajax, name='ajax_read'),
+    # This logic migrated into REST API: api/views.py
+    # path('ajax_read/<int:pk>', code_read_ajax, name='ajax_read'),
 ]
