db6ef729d876b121732bb2bbccdb45712a3199e0
Andrey Kiselev <ezhichek@yandex-team.ru>
2019-07-10 16:44:00 +0300

    create short link

--- executor/executor/urls.py
+++ executor/executor/urls.py
@@ -1,22 +1,9 @@
-"""executor URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, reverse_lazy
 from django.views.generic import RedirectView
+
+from mainapp.views import ShortURLRedirect
 
 urlpatterns = [
     path('', RedirectView.as_view(url=reverse_lazy('mainapp:index'),
@@ -24,6 +11,7 @@ urlpatterns = [
     path('code/', include('mainapp.urls', namespace='mainapp')),
     path('auth/', include('authapp.urls', namespace='authapp')),
     path('api/', include('api.urls', namespace='api')),
+    path('s/<slug:link>', ShortURLRedirect.as_view(), name='short_link'),
 
     path('admin/', admin.site.urls),
 ]
--- executor/mainapp/views.py
+++ executor/mainapp/views.py
@@ -1,8 +1,9 @@
 from django.urls import reverse
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, RedirectView
 
 from mainapp.forms import CodeBaseForm
 from mainapp.models import CodeBase
+from mainapp.utils import ShortURL
 
 
 class CodeCreate(CreateView):
@@ -36,9 +37,17 @@ class CodeRead(DetailView):
 
         return context
 
+
 # This logic migrated into REST API: api/views.py
 # def code_read_ajax(request, pk):
 #     if request.is_ajax():
 #         qs = get_object_or_404(CodeExecution, pk=pk)
 #         return JsonResponse({'output': qs.output,
 #                              'has_errors': qs.has_errors})
+
+
+class ShortURLRedirect(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        link = self.kwargs.get('link')
+        codebase_pk = ShortURL().decode(link)
+        return reverse('mainapp:read', kwargs={'pk': codebase_pk})
